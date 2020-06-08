e48c619d80eef5bfc0645208bf0c42072bde22fd
Manumental32 <manuel.aquino.utn@gmail.com>
2020-06-08 19:15:00 -0300

    Updates

---
+++ precache-manifest.ff42326246cc4476c132f3326f3ad322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95a52d38819fd46dd915f3f474d9ad1",
+    "url": "/Manumental32/getting-started-with-react/index.html"
+  },
+  {
+    "revision": "b5ddf882410970e03a7c",
+    "url": "/Manumental32/getting-started-with-react/static/css/main.53574754.chunk.css"
+  },
+  {
+    "revision": "016172e848e1ad467c3c",
+    "url": "/Manumental32/getting-started-with-react/static/js/2.ca98edaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Manumental32/getting-started-with-react/static/js/2.ca98edaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5ddf882410970e03a7c",
+    "url": "/Manumental32/getting-started-with-react/static/js/main.17db5288.chunk.js"
+  },
+  {
+    "revision": "33baf6b72ffa864cb874",
+    "url": "/Manumental32/getting-started-with-react/static/js/runtime-main.ef36f626.js"
+  }
+]);
